1b03185f4aa3c94d5c06d8db674bb0d3dcc57312
bilikhasan <fatherlyy@hotmail.com.tr>
2024-09-19 21:52:46 +0300

    SkillController için LIST - CREATE - DELETE - UPDATE işlemleri eklendi

---
+++ MyPortfolioProjectNight/Controllers/SkillController.cs
@@ -0,0 +1,48 @@
+﻿using MyPortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolioProjectNight.Controllers
+{
+    public class SkillController : Controller
+    {
+        // GET: Skill
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+
+        [HttpGet]                                                       //Sayfa yuklenınce
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]                                                      //Sayfa post olunca yanı button tıklanınca
+        public ActionResult CreateSkill(Skill skill)
+        {
+            context.Skill.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            context.Skill.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            return View(value);
+        }
+    }
+}
