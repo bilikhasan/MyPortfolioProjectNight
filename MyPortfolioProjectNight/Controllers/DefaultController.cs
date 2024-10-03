9cddff6ec20712434ded806c1c7bb6ce69e24155
bilikhasan <fatherlyy@hotmail.com.tr>
2024-10-03 22:01:48 +0300

    ContactController eklendi.

--- MyPortfolioProjectNight/Controllers/DefaultController.cs
+++ MyPortfolioProjectNight/Controllers/DefaultController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyPortfolioProjectNight.Models;
+
 
 namespace MyPortfolioProjectNight.Controllers
 {
     public class DefaultController : Controller
     {
+
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
         // GET: Default
         public ActionResult Index()
         {
@@ -24,7 +28,33 @@ namespace MyPortfolioProjectNight.Controllers
         public PartialViewResult PartialNavbar()
         {
             return PartialView();
-
         }
+        public PartialViewResult PartialHeader()
+        {
+            ViewBag.title = context.Profile.Select(x=>x.Title).FirstOrDefault();
+            ViewBag.description = context.Profile.Select(x=>x.Description).FirstOrDefault();
+            ViewBag.address = context.Profile.Select(x=>x.Address).FirstOrDefault();
+            ViewBag.email = context.Profile.Select(x=>x.Email).FirstOrDefault();
+            ViewBag.phone = context.Profile.Select(x=>x.Phone).FirstOrDefault();
+            ViewBag.github = context.Profile.Select(x=>x.Github).FirstOrDefault();
+            ViewBag.imageurl = context.Profile.Select(x=>x.ImageUrl).FirstOrDefault();
+            return PartialView();
+        }
+        public PartialViewResult PartialAbout()
+        {
+            var values = context.About.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialExperience()
+        {
+            var values = context.Experience.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialSkill()
+        {
+            var values = context.Skill.Where(x => x.Status == true).ToList();
+            return PartialView(values);
+        }
+
     }
 }
