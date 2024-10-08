5f58dc75642933f576f793756ed16e1ebab47862
bilikhasan <fatherlyy@hotmail.com.tr>
2024-10-08 17:08:29 +0300

    Chart eklendi

---
+++ MyPortfolioProjectNight/Controllers/ChartController.cs
@@ -0,0 +1,27 @@
+﻿using MyPortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolioProjectNight.Controllers
+{
+    public class ChartController : Controller
+    {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+
+        public ActionResult Index()
+        {
+            var skills = context.Skill.ToList();
+            
+            var skillNames = context.Skill.Select(x=>x.SkillName).ToList();
+            var skillRates = context.Skill.Select(x=>x.Rate).ToList();
+
+            ViewBag.SkillNames = skillNames;
+            ViewBag.SkillRates = skillRates;
+
+            return View();
+        }
+    }
+}
