e9ad43dd1de500a2ec2812efb61322f8e140adca
bilikhasan <fatherlyy@hotmail.com.tr>
2024-10-12 15:50:15 +0300

    Eklemeler yapıldı.

--- MyPortfolioProjectNight/Controllers/CategoryController.cs
+++ MyPortfolioProjectNight/Controllers/CategoryController.cs
@@ -1,21 +1,60 @@
-﻿using System;
+﻿using MyPortfolioProjectNight.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MyPortfolioProjectNight.Controllers
+namespace PortfolioProject.Controllers
 {
     public class CategoryController : Controller
     {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
 
         public ActionResult CategoryList()
         {
-            return View();
+            var values = context.Category.ToList();
+            return View(values);
         }
+
+        [HttpGet]
         public ActionResult CreateCategory()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            context.Category.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var value = context.Category.Find(id);
+            context.Category.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            var value = context.Category.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            var value = context.Category.Find(category.CategoryId);
+
+            value.CategoryName = category.CategoryName;
+            context.SaveChanges();
+
+            return RedirectToAction("CategoryList");
         }
     }
 }
---
+++ MyPortfolioProjectNight/Controllers/PortfolioController.cs
@@ -0,0 +1,65 @@
+﻿using MyPortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProject.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+
+        public ActionResult PortfolioList()
+        {
+            var values = context.Portfolio.ToList();
+
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreatePortfolio()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePortfolio(Portfolio portfolio)
+        {
+            context.Portfolio.Add(portfolio);
+            context.SaveChanges();
+
+            return RedirectToAction("PortfolioList");
+        }
+
+        public ActionResult DeletePortfolio(int id)
+        {
+            var value = context.Portfolio.Find(id);
+            context.Portfolio.Remove(value);
+            context.SaveChanges();
+
+            return RedirectToAction("PortfolioList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdatePortfolio(int id)
+        {
+            var value = context.Portfolio.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdatePortfolio(Portfolio portfolio)
+        {
+            var value = context.Portfolio.Find(portfolio.PortfolioID);
+            
+            value.PortfolioTitle = portfolio.PortfolioTitle;
+            value.PortfolioSubTitle = portfolio.PortfolioSubTitle;
+            value.PortfolioImageUrl = portfolio.PortfolioImageUrl;
+            
+            context.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+    }
+}
