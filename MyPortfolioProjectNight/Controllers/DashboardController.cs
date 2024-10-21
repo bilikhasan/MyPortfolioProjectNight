5676d731d6d40719f83805d06710bb968de26c7c
bilikhasan <fatherlyy@hotmail.com.tr>
2024-10-21 14:47:33 +0300

    Son

---
+++ MyPortfolioProjectNight/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyPortfolioProjectNight.Models;
+
+namespace MyPortfolioProjectNight.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+        public ActionResult Index()
+        {
+            ViewBag.EducationCount = context.Education.Count();
+            ViewBag.SkillCount = context.Skill.Count();
+            ViewBag.ExperienceCount = context.Experience.Count();
+            ViewBag.ServiceCount = context.Service.Count();
+            ViewBag.SocialMediaCount = context.SocialMedia.Count();
+            ViewBag.MessageCount = context.Contact.Count();
+
+            return View();
+        }
+    }
+}
