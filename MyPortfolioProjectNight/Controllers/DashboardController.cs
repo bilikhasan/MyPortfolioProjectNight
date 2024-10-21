5a88cac5968fb4c906ddc045fa5f211a1461a2c0
bilikhasan <fatherlyy@hotmail.com.tr>
2024-10-21 14:49:39 +0300

    q

--- MyPortfolioProjectNight/Controllers/DashboardController.cs
+++ MyPortfolioProjectNight/Controllers/DashboardController.cs
@@ -14,7 +14,7 @@ namespace MyPortfolioProjectNight.Controllers
         public ActionResult Index()
         {
             ViewBag.EducationCount = context.Education.Count();
-            ViewBag.SkillCount = context.Skill.Count();
+            ViewBag.SkillCount = context.Skill.Count(); 
             ViewBag.ExperienceCount = context.Experience.Count();
             ViewBag.ServiceCount = context.Service.Count();
             ViewBag.SocialMediaCount = context.SocialMedia.Count();
